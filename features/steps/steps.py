9c46353d231df9694aa76d7e9d1ac0b3a4717910
Diego <jesustello192002@gmail.com>
2025-04-30 10:59:15 -0500

    Iniciamos el repositorio y configuración de CI

---
+++ features/steps/steps.py
@@ -0,0 +1,9 @@
+from behave import given, then
+
+@given('un paso vacio')
+def step_impl(context):
+    pass
+
+@then('nada ocurre')
+def step_impl(context):
+    pass
---
+++ features/vacio.feature
@@ -0,0 +1,7 @@
+# language: es
+
+Característica: Dia 0
+
+  Escenario: ninguna accion requerida
+    Dado un paso vacio
+    Entonces nada ocurre
